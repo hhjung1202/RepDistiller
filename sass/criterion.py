d2fbc42e5b6d70182d6322a43770ea2467bb2c95
HyoungHo Jung <hhjung1202@naver.com>
2020-10-21 03:33:23 +0900

    error

--- sass/criterion.py
+++ sass/criterion.py
@@ -7,7 +7,7 @@ class StLoss(nn.Module):
         super(StLoss, self).__init__()
         self.criterion_s = StyleLoss()
 
-    def forward(self, st_mse, st_label, weight=1e+1):
+    def forward(self, st_mse, st_label, weight=1e-1):
         loss = self.criterion_s(st_mse, st_label, weight)
         return loss
 
@@ -17,24 +17,13 @@ class StyleLoss(nn.Module):
         self.softmin = nn.Softmin(dim=-1)
 
     def forward(self, input, target, weight):
-        # likelihood = self.softmin(input * weight)
+        likelihood = self.softmin(input * weight)
         # # print(self.softmin(input * 1e+5).var(dim=1).mean())
         # # print(self.softmin(input * 1e+4).var(dim=1).mean())
         # # print(self.softmin(input * 1e+3).var(dim=1).mean())
         # # print(self.softmin(input * 1e+2).var(dim=1).mean())
         # # print(self.softmin(input * 1e+1).var(dim=1).mean())
         # # print(self.softmin(input).var(dim=1).mean())
-        # log_likelihood = likelihood.log()
-        # nll_loss = F.nll_loss(log_likelihood, target)
-        # return nll_loss
-
-        likelihood = F.softmax(input * weight, dim=-1)
-        # print(F.softmax(input * 1e+5, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input * 1e+4, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input * 1e+3, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input * 1e+2, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input * 1e+1, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input, dim=-1).var(dim=1).mean())
-        log_likelihood_reverse = torch.log(1 - likelihood)
-        nll_loss = F.nll_loss(log_likelihood_reverse, target)
+        log_likelihood = likelihood.log()
+        nll_loss = F.nll_loss(log_likelihood, target)
         return nll_loss
