44b2bf5e833b2c3c011e6e46438e14e4a9982456
HyoungHo Jung <hhjung1202@naver.com>
2020-10-21 03:12:53 +0900

    error

--- sass/criterion.py
+++ sass/criterion.py
@@ -6,7 +6,7 @@ class StLoss(nn.Module):
         super(StLoss, self).__init__()
         self.criterion_s = StyleLoss()
 
-    def forward(self, st_mse, st_label, weight=1e-1):
+    def forward(self, st_mse, st_label, weight=1e+2):
         loss = self.criterion_s(st_mse, st_label, weight)
         return loss
 
@@ -17,12 +17,12 @@ class StyleLoss(nn.Module):
 
     def forward(self, input, target, weight):
         likelihood = self.softmin(input * weight)
-        print(self.softmin(input * 1e+5).var(dim=1).mean())
-        print(self.softmin(input * 1e+4).var(dim=1).mean())
-        print(self.softmin(input * 1e+3).var(dim=1).mean())
-        print(self.softmin(input * 1e+2).var(dim=1).mean())
-        print(self.softmin(input * 1e+1).var(dim=1).mean())
-        print(self.softmin(input).var(dim=1).mean())
+        # print(self.softmin(input * 1e+5).var(dim=1).mean())
+        # print(self.softmin(input * 1e+4).var(dim=1).mean())
+        # print(self.softmin(input * 1e+3).var(dim=1).mean())
+        # print(self.softmin(input * 1e+2).var(dim=1).mean())
+        # print(self.softmin(input * 1e+1).var(dim=1).mean())
+        # print(self.softmin(input).var(dim=1).mean())
         log_likelihood = likelihood.log()
         nll_loss = F.nll_loss(log_likelihood, target)
         return nll_loss
