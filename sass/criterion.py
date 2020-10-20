7ac26a62083bdfa63657fbccb78041155377ef3c
HyoungHo Jung <hhjung1202@naver.com>
2020-10-21 03:28:43 +0900

    error

--- sass/criterion.py
+++ sass/criterion.py
@@ -29,12 +29,12 @@ class StyleLoss(nn.Module):
         # return nll_loss
 
         likelihood = F.softmax(input * weight, dim=-1)
-        # print(F.softmax(input * 1e-5, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input * 1e-4, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input * 1e-3, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input * 1e-2, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input * 1e-1, dim=-1).var(dim=1).mean())
-        # print(F.softmax(input, dim=-1).var(dim=1).mean())
+        print(F.softmax(input * 1e+5, dim=-1).var(dim=1).mean())
+        print(F.softmax(input * 1e+4, dim=-1).var(dim=1).mean())
+        print(F.softmax(input * 1e+3, dim=-1).var(dim=1).mean())
+        print(F.softmax(input * 1e+2, dim=-1).var(dim=1).mean())
+        print(F.softmax(input * 1e+1, dim=-1).var(dim=1).mean())
+        print(F.softmax(input, dim=-1).var(dim=1).mean())
         log_likelihood_reverse = torch.log(1 - likelihood)
         nll_loss = F.nll_loss(log_likelihood_reverse, target)
         return nll_loss
